df66c8484b98e5493708db1e9e977d263e0f4de8
tuanthanhhh <letuanthanh.work@gmail.com>
2024-09-26 12:24:24 +0700

    final project

--- ESP8266/esp32/esp32.ino
+++ ESP8266/esp32/esp32.ino
@@ -1,85 +1,118 @@
-//#include <HardwareSerial.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
+/* This code works with MAX30102 + 128x32 OLED i2c + Buzzer and Arduino UNO
+ * It's displays the Average BPM on the screen, with an animation and a buzzer sound
+ * everytime a heart pulse is detected
+ * It's a modified version of the HeartRate library example
+ * Refer to www.surtrtech.com for more details or SurtrTech YouTube channel
+ */
 
-// Thông tin WiFi
-const char* ssid = "ThanhLe";
-const char* password = "123456789";
+#include <Adafruit_GFX.h>        //OLED libraries
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+#include "MAX30105.h"           //MAX3010x library
+#include "heartRate.h"          //Heart rate calculating algorithm
 
-// Thông tin MQTT cho ThingBoard
-const char* mqtt_server = "thingsboard.cloud";
-const char* access_token = "092Pp7Vzcrwmg2zFbL6l";  // Token của thiết bị trên ThingBoard
+MAX30105 particleSensor;
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+float beatsPerMinute;
+int beatAvg;
 
-//HardwareSerial SerialSTM(2);  // Sử dụng UART2
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 
-void setup_wifi() {
-  delay(10);
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); //Declaring the display name (display)
 
-  WiFi.begin(ssid, password);
+static const unsigned char PROGMEM logo2_bmp[] =
+{ 0x03, 0xC0, 0xF0, 0x06, 0x71, 0x8C, 0x0C, 0x1B, 0x06, 0x18, 0x0E, 0x02, 0x10, 0x0C, 0x03, 0x10,              //Logo2 and Logo3 are two bmp pictures that display on the OLED if called
+0x04, 0x01, 0x10, 0x04, 0x01, 0x10, 0x40, 0x01, 0x10, 0x40, 0x01, 0x10, 0xC0, 0x03, 0x08, 0x88,
+0x02, 0x08, 0xB8, 0x04, 0xFF, 0x37, 0x08, 0x01, 0x30, 0x18, 0x01, 0x90, 0x30, 0x00, 0xC0, 0x60,
+0x00, 0x60, 0xC0, 0x00, 0x31, 0x80, 0x00, 0x1B, 0x00, 0x00, 0x0E, 0x00, 0x00, 0x04, 0x00,  };
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+static const unsigned char PROGMEM logo3_bmp[] =
+{ 0x01, 0xF0, 0x0F, 0x80, 0x06, 0x1C, 0x38, 0x60, 0x18, 0x06, 0x60, 0x18, 0x10, 0x01, 0x80, 0x08,
+0x20, 0x01, 0x80, 0x04, 0x40, 0x00, 0x00, 0x02, 0x40, 0x00, 0x00, 0x02, 0xC0, 0x00, 0x08, 0x03,
+0x80, 0x00, 0x08, 0x01, 0x80, 0x00, 0x18, 0x01, 0x80, 0x00, 0x1C, 0x01, 0x80, 0x00, 0x14, 0x00,
+0x80, 0x00, 0x14, 0x00, 0x80, 0x00, 0x14, 0x00, 0x40, 0x10, 0x12, 0x00, 0x40, 0x10, 0x12, 0x00,
+0x7E, 0x1F, 0x23, 0xFE, 0x03, 0x31, 0xA0, 0x04, 0x01, 0xA0, 0xA0, 0x0C, 0x00, 0xA0, 0xA0, 0x08,
+0x00, 0x60, 0xE0, 0x10, 0x00, 0x20, 0x60, 0x20, 0x06, 0x00, 0x40, 0x60, 0x03, 0x00, 0x40, 0xC0,
+0x01, 0x80, 0x01, 0x80, 0x00, 0xC0, 0x03, 0x00, 0x00, 0x60, 0x06, 0x00, 0x00, 0x30, 0x0C, 0x00,
+0x00, 0x08, 0x10, 0x00, 0x00, 0x06, 0x60, 0x00, 0x00, 0x03, 0xC0, 0x00, 0x00, 0x01, 0x80, 0x00  };
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
 
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if (client.connect("ESP32_Client", access_token, NULL)) {
-      Serial.println("connected");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
-    }
-  }
-}
+void setup() {  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //Start the OLED display
+  display.display();
+  delay(3000);
+  // Initialize sensor
+  particleSensor.begin(Wire, I2C_SPEED_FAST); //Use default I2C port, 400kHz speed
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
 
-void setup() {
-  Serial.begin(115200);  // Giao tiếp với máy tính
-  //Serial2.begin(9600, SERIAL_8N1, 16, 17);  // TX2 là GPIO 17, RX2 là GPIO 16
-
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
-  if (Serial.available()) {
-    String data = Serial.readStringUntil('\n');  // Đọc dữ liệu từ STM32
-    int heart_rate, spo2;
-    sscanf(data.c_str(), "%d,%d", &heart_rate, &spo2);
+ long irValue = particleSensor.getIR();    //Reading the IR value it will permit us to know if there's a finger on the sensor or not
+                                           //Also detecting a heartbeat
+if(irValue > 7000){                                           //If a finger is detected
+    display.clearDisplay();                                   //Clear the display
+    display.drawBitmap(5, 5, logo2_bmp, 24, 21, WHITE);       //Draw the first bmp picture (little heart)
+    display.setTextSize(2);                                   //Near it display the average BPM you can display the BPM if you want
+    display.setTextColor(WHITE); 
+    display.setCursor(50,0);                
+    display.println("BPM");             
+    display.setCursor(50,18);                
+    display.println(beatAvg); 
+    display.display();
     
-    Serial.println(heart_rate);
-    Serial.println(spo2);
+  if (checkForBeat(irValue) == true)                        //If a heart beat is detected
+  {
+    display.clearDisplay();                                //Clear the display
+    display.drawBitmap(0, 0, logo3_bmp, 32, 32, WHITE);    //Draw the second picture (bigger heart)
+    display.setTextSize(2);                                //And still displays the average BPM
+    display.setTextColor(WHITE);             
+    display.setCursor(50,0);                
+    display.println("BPM");             
+    display.setCursor(50,18);                
+    display.println(beatAvg); 
+    display.display();
+    tone(3,1000);                                        //And tone the buzzer for a 100ms you can reduce it it will be better
+    delay(100);
+    noTone(3);                                          //Deactivate the buzzer to have the effect of a "bip"
+    //We sensed a beat!
+    long delta = millis() - lastBeat;                   //Measure duration between two beats
+    lastBeat = millis();
 
-    // Định dạng dữ liệu JSON để gửi lên ThingBoard
-   // String payload = "{\"heart_rate\":" + heart_rate + \"spo2"\+ spo2 "\}";
-    String payload = "{\"heart_rate\": " + String(heart_rate) + ", \"spo2\": " + String(spo2) + "}";
+    beatsPerMinute = 60 / (delta / 1000.0);           //Calculating the BPM
 
-    // Gửi dữ liệu lên ThingBoard
-    if (client.publish("v1/devices/me/telemetry", payload.c_str())) {
-      Serial.println("Data sent to ThingBoard");
-    } else {
-      Serial.println("Failed to send data");
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)               //To calculate the average we strore some values (4) then do some math to calculate the average
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
     }
   }
-  //delay(500);  // Đọc và gửi mỗi giây
+
+}
+  if (irValue < 7000){       //If no finger is detected it inform the user and put the average BPM to 0 or it will be stored for the next measure
+     beatAvg=0;
+     display.clearDisplay();
+     display.setTextSize(1);                    
+     display.setTextColor(WHITE);             
+     display.setCursor(30,5);                
+     display.println("Please Place "); 
+     display.setCursor(30,15);
+     display.println("your finger ");  
+     display.display();
+     noTone(3);
+     }
+
 }
